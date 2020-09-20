1e0d603492781120db4a0a8f0ebd21efadd04b5b
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-20 02:04:08 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -25,8 +25,8 @@ class App < Sinatra::Base
   
   
     get '/:operation/:number1/:number2' do 
-      number1 = param[:number1].to_i 
-      number2 = param[:number2].to_i 
+      number1 = params[:number1].to_i 
+      number2 = params[:number2].to_i 
       
       answer = "Unable to perform this operation"
       
