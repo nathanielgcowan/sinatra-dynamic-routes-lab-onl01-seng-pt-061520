f98a3e844f10f28b0a6d78d2e21ace835571105a
nathanielgcowan <nathaniel.g.cowan@gmail.com>
2020-09-20 01:59:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -24,7 +24,10 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do 
-    number1 = param[:number1].to_i
+    number1 = param[:number1].to_i 
+    number2 = param[:number2].to_i 
+    
+    
   end
 
 end
